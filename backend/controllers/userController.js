7424112bbcd34886f138bded2ebc4e6b0703613d
Jason Xie <thejasonxie@gmail.com>
2022-06-06 20:26:34 -0400

    Added DB connection and user schema with registration

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,7 @@
 const asyncHandler = require('express-async-handler')
+const bcrypt = require('bcryptjs')
+
+const User = require('../models/userModel')
 
 // @desc    Register a new user
 // @route   /api/users
@@ -12,10 +15,32 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new Error("Please include all fields")
     }
 
-    console.log("Name " + name + " Email " + email + " Password " + password)
+    const userExists = await User.findOne({ email })
 
-    res.send("Register Route!")
-    
+    if (userExists) {
+        res.status(400)
+        throw new Error("User already exists")
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    })
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            user: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(400)
+        throw new Error("Invalid user data")
+    }    
 })
 
 // @desc    Login a user
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
 const { errorHandler } = require("./middleware/errorMiddleware.js")
+const connectDB = require('./config/db')
+
 const PORT = process.env.PORT || 5000
+
+connectDB()
 
 const app = express()
 
