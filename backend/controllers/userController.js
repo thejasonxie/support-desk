bc0a70a5cef27d5a05f8001252a6ea78d1bcbd99
Jason Xie <thejasonxie@gmail.com>
2022-06-07 19:15:28 -0400

    Added JWT middleware to protect private routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const User = require('../models/userModel')
 
@@ -35,7 +36,8 @@ const registerUser = asyncHandler(async (req, res) => {
         res.status(201).json({
             _id: user._id,
             user: user.name,
-            email: user.email
+            email: user.email,
+            token: generateToken(user._id)
         })
     } else {
         res.status(400)
@@ -47,7 +49,40 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route   /api/login
 // @access  Public
 const loginUser = asyncHandler(async (req, res) => {
-    res.send("Login Route")
+    const {email, password} = req.body
+
+    const user = await User.findOne({email})
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+        res.status(200).json({
+            _id: user._id,
+            user: user.name,
+            email: user.email,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(401)
+        throw new Error("Invalid credentials")
+    }
+
 })
 
-module.exports = { registerUser, loginUser }
+// @desc    Get current user
+// @route   /api/users/me
+// @access  Private
+const getMe = asyncHandler(async (req, res) => {
+    const user = {
+        id: req.user._id,
+        email: req.user.email,
+        name: req.user.name
+    }
+    res.status(200).json(user)
+})
+
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '30d'
+    })
+}
+
+module.exports = { registerUser, loginUser, getMe }
