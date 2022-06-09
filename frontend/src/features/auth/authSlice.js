597243a28626055f955d17b143ec0ad2a426541a
Jason Xie <thejasonxie@gmail.com>
2022-06-08 20:47:55 -0400

    Implemented logout

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -33,6 +33,10 @@ export const login = createAsyncThunk("auth/login", async (user, thunkAPI) => {
     console.log(user);
 });
 
+export const logout = createAsyncThunk("auth/logout", async () => {
+    await authService.logout();
+});
+
 export const authSlice = createSlice({
     name: "auth",
     initialState,
@@ -58,6 +62,9 @@ export const authSlice = createSlice({
                 state.isLoading = false;
                 state.isError = true;
                 state.message = action.payload;
+                state.user = null;
+            })
+            .addCase(logout.fulfilled, (state) => {
                 state.user = null;
             });
     },
