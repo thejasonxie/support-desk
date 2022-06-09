025702645ae87bb09173c568f6defd58cc674471
Jason Xie <thejasonxie@gmail.com>
2022-06-09 11:59:25 -0400

    Implemented ticket list view

--- backend/models/ticketModel.js
+++ backend/models/ticketModel.js
@@ -16,7 +16,7 @@ const ticketSchema = mongoose.Schema(
             type: String,
             required: [true, "Please enter a description of the issue"],
         },
-        isAdmin: {
+        status: {
             type: String,
             required: true,
             enum: ["new", "open", "closed"],
